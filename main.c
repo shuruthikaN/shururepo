4cf1a97f9585786aae9e8e0a6e72a0fca2078c3b
shuruthikaN <shuruthika.n@yantravision.com>
2025-12-15 15:57:01 +0530

    code

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* Function prototypes */
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+
+int main() {
+    int a, b, choice;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n1.Add\n2.Sub\n3.Mul\n4.Div\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("Result = %d\n", add(a, b));
+            break;
+        case 2:
+            printf("Result = %d\n", sub(a, b));
+            break;
+        case 3:
+            printf("Result = %d\n", mul(a, b));
+            break;
+        case 4:
+            printf("Result = %d\n", div(a, b));
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+
+
+    return 0;
+}
+
